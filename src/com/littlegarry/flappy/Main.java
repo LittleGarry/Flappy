83ece04c88cfd6bc362629a10176a58c10edfa4f
LittleGarry <bulgakov_egor@outlook.com>
2019-05-19 15:13:11 +0300

    Created Display and context

--- src/com/littlegarry/flappy/Main.java
+++ src/com/littlegarry/flappy/Main.java
@@ -1,6 +1,12 @@
 package com.littlegarry.flappy;
 
-public class Main implements Runnable{
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
+
+public class Main implements Runnable {
 	
 	private int width = 1280;
 	private int height = 720;
@@ -16,12 +22,26 @@ public class Main implements Runnable{
 	}
 	
 	public void run() {
-		Display.setDisplayMode(new DisplayMode(width, height));
+		try {
+			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.setTitle(title);
+			ContextAttribs context = new ContextAttribs(3, 3);
+			if (System.getProperty("os.name").contains("Mac")) context = new ContextAttribs(3, 2);
+			Display.create(new PixelFormat(), context.withProfileCore(true));
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		while (running) {
+			Display.update();
+			if (Display.isCloseRequested())	running = false;
+		}
+		
+		Display.destroy();
 	}
 	
 	public static void main(String[] args) {
-		
-	
+		new Main().start();
 	}
 	
 }
