9ac32d2e8caf54f4868909858a817e3db3d9f508
LittleGarry <bulgakov_egor@outlook.com>
2019-05-20 23:06:05 +0300

    Java 8
    # Conflicts:
    #	.classpath
    #	.settings/org.eclipse.jdt.core.prefs
    #	bin/com/littlegarry/flappy/Main.class
    #	src/com/littlegarry/flappy/Main.java

--- src/com/littlegarry/flappy/Main.java
+++ src/com/littlegarry/flappy/Main.java
@@ -4,9 +4,8 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.PixelFormat;
 
-public class Main implements Runnable {
+public class Main implements Runnable{
 	
 	private int width = 1280;
 	private int height = 720;
@@ -25,22 +24,20 @@ public class Main implements Runnable {
 		try {
 			Display.setDisplayMode(new DisplayMode(width, height));
 			Display.setTitle(title);
-			ContextAttribs context = new ContextAttribs(3, 3);
-			if (System.getProperty("os.name").contains("Mac")) context = new ContextAttribs(3, 2);
-			Display.create(new PixelFormat(), context.withProfileCore(true));
-		} catch (LWJGLException e) {
+			Display.create();
+		} catch (LWJGLException e) { 
 			e.printStackTrace();
 		}
 		
 		while (running) {
 			Display.update();
-			if (Display.isCloseRequested())	running = false;
+			if (Display.isCloseRequested()) running = false;
 		}
 		
 		Display.destroy();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] argc) {
 		new Main().start();
 	}
 	
