885e4f8f5b3efd199473f9be7e76305c6210ff7e
LittleGarry <bulgakov_egor@outlook.com>
2019-05-31 20:46:06 +0300

    Extended input to GLFWKeyCallback

---
+++ src/com/littlegarry/flappy/input/Input.java
@@ -0,0 +1,13 @@
+package com.littlegarry.flappy.input;
+
+import org.lwjgl.glfw.GLFWKeyCallback;
+
+public class Input extends GLFWKeyCallback{
+
+	@Override
+	public void invoke(long window, int key, int scancode, int action, int mods) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/littlegarry/flappy/input/input.java
+++
@@ -1,5 +0,0 @@
-package com.littlegarry.flappy.input;
-
-public class input {
-
-}
