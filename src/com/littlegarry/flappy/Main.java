8a8b49fe7506385f5221222f30fa441baba634df
LittleGarry <bulgakov_egor@outlook.com>
2019-06-06 23:03:40 +0300

    Slowed down bg speed

--- src/com/littlegarry/flappy/Main.java
+++ src/com/littlegarry/flappy/Main.java
@@ -72,9 +72,35 @@ public class Main implements Runnable{
 	public void run() {
 		init();
 		
+		long lastTime = System.nanoTime();
+		double delta = 0.0;
+		double ns = 1000000000 / 60.0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		
 		while (running) {
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			if (delta >= 1.0) {
+				update();
+				updates++;
+				delta--;				
+			}
+			
 			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				
+				System.out.println(updates + " ups, " + frames + " fps");
+				
+				updates = 0;
+				frames = 0;
+			}
 			
 			if (glfwWindowShouldClose(window))
 				running = false;
